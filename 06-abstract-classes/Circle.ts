25ddb0ced3963c6b1ec36a09bc4887af8f665b79
Azhar Uddin Azad <azhar.u.azad@gmail.com>
2020-03-08 16:57:45 +0600

    typescript abstract classes

---
+++ 06-abstract-classes/Circle.ts
@@ -0,0 +1,28 @@
+import { Shape } from "./Shape";
+
+export class Circle extends Shape {
+
+  constructor(
+    private theX: number,
+    private theY: number,
+    private _radius: number
+  ) {
+    super(theX, theY);
+  }
+
+  public get radius(): number {
+    return this._radius;
+  }
+
+  public set radius(rad: number) {
+    this._radius = rad;
+  }
+
+  public getInfo(): string {
+    return `${super.getInfo()}, radius: ${this._radius}`;
+  }
+  
+  calculateArea(): number {
+    return Math.PI * Math.pow(this._radius, 2);
+  }
+}
